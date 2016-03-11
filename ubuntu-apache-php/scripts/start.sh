88605ca3397cdb65a0325eb1a253342ecba1c94f
Constantinos Kouloumbris <constantinoskouloumbris@rentalcars.com>
2016-03-11 15:25:15 +0000

    fixed config path

--- ubuntu-apache-php/scripts/start.sh
+++ ubuntu-apache-php/scripts/start.sh
@@ -13,14 +13,9 @@ fi
 
 # Display PHP error's or not
 if [[ "$ERRORS" != "1" ]] ; then
-  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/5.6/fpm/php.ini
-  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/5.6/fpm/php.ini
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/5.6/apache2/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/5.6/apache2/php.ini
 fi
-
-# Tweak nginx to match the workers to cpu's
-
-procs=$(cat /proc/cpuinfo |grep processor | wc -l)
-sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Very dirty hack to replace variables in code with ENVIRONMENT values
 if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
